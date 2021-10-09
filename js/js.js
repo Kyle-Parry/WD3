9756507bc88f65bb5db840af9e6e8b6e11adb261
Shockor <kyleparry01@gmail.com>
2021-10-09 13:13:10 +1000

    form done

---
+++ js/js.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    let myTimepicker = document.getElementById("reservation-time");
+    myTimepicker.addEventListener("change", function() {
+    
+      console.log(`User changed the value to ${myTimepicker.value}`);
+      
+      let [hours, minutes] = myTimepicker.value.split(":");
+      
+      minutes = (Math.ceil(minutes / 15) * 15);
+      if (minutes == 0) minutes = "00";
+      if (minutes == 60) { minutes = "00"; ++hours % 24; }
+          
+      let newValue = hours + ":" + minutes;
+      
+      console.log(`Rounding value to ${newValue}`);
+      
+      myTimepicker.value = newValue;
+    });
+  }
